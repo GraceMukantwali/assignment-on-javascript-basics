af802a3c7d75cf8678ad2089a62e848a12023e2c
GraceMukantwali <megagradus@gmail.com>
2023-11-14 16:35:52 +0200

    question1

---
+++ Q1.js
@@ -0,0 +1,5 @@
+function addNumbers(a, b) {
+    return a + b;
+  }
+   var result = addNumbers(2,4);
+   console.log(result);
---
+++ q2.js
@@ -0,0 +1,5 @@
+function multiplyNumbers(a, b) {
+    return a * b;
+  }
+  var result=multiplyNumbers(2,4);
+ console.log (result);
